a0101dabb837c50d1e7e0593e21f432e482b6a1f
Sheil Sarda <sheilsarda@yahoo.com>
2021-01-15 23:28:08 -0500

    logic for solution

--- Embedded Systems/dispatch.c
+++ Embedded Systems/dispatch.c
@@ -7,10 +7,17 @@
 // Handle Display messages
 void display_message(uint8_t *m, uint16_t message_len){
 
+    for(int i = 0; i < message_len; ++i)
+      printf("%c ", m[i]);
+
+    printf("\r\n");
 }
 
 // Handle Motor messages
 void update_motor(float forward_back, float left_right){
+
+  printf("Forward Back %d \r\n", forward_back);
+  printf("Left Right   %d \r\n", left_right);
 
 }
 
@@ -20,11 +27,30 @@ void dispatcher(uint8_t *packet){
   uint8_t  message_type = packet[1];
   uint16_t message_len  = packet[2] || (packet[3] << sizeof(uint8_t));
 
-  uint8_t message_data[message_len];
 
   printf("Packet ID %d \r\n", packet_id);
   printf("Type      %x \r\n", message_type);
   printf("Length    %d \r\n", message_len);
+
+  if(message_type == DISPLAY_MESSAGE){
+    uint8_t message[message_len];
+    for(int i = 0; i < message_len; ++i)
+      message[i] = packet[4 + i];
+
+    display_message(message, message_len);
+
+  } else {
+    float forward_back = (float) packet[4];
+    float left_right   = (float) packet[8];
+
+    for(int i = 1; i < 4; ++i){
+      forward_back |= (float) (packet[4 + i] << sizeof(uint8_t) * i);
+      left_right   |= (float) (packet[8 + i] << sizeof(uint8_t) * i);
+    }
+
+    update_motor(forward_back, left_right); 
+    
+  }
 }
 
 
