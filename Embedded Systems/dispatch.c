5c33c08d11273b4f8be7af216ad54b0ce8ab666e
Sheil Sarda <sheilsarda@yahoo.com>
2021-01-15 22:24:16 -0500

    dispatch reads and prints data

--- Embedded Systems/dispatch.c
+++ Embedded Systems/dispatch.c
@@ -1,26 +1,41 @@
+#include <stdint.h>
+#include <stdio.h>
 #define DISPLAY_MESSAGE 0x34
 #define MOTOR_MESSAGE 0x80
 
 
 // Handle Display messages
-int display_message(uint8_t *m, uint16_t message_len){
+void display_message(uint8_t *m, uint16_t message_len){
 
-  return 1;
 }
 
 // Handle Motor messages
-int update_motor(float forward_back, float left_right){
+void update_motor(float forward_back, float left_right){
 
-  return 1;
 }
 
-int dispatcher(uint8_t packet){
+void dispatcher(uint8_t *packet){
   
-  uint8_t  packet_id      = packet[0];
-  uint8_t  message_type   = packet[1];
-  uint16_t message_length = packet[2] || (packet[3] << sizeof(uint8_t));
+  uint8_t  packet_id    = packet[0];
+  uint8_t  message_type = packet[1];
+  uint16_t message_len  = packet[2] || (packet[3] << sizeof(uint8_t));
 
-  uint8_t message_data[message_length];
+  uint8_t message_data[message_len];
+
+  printf("Packet ID %d \r\n", packet_id);
+  printf("Type      %x \r\n", message_type);
+  printf("Length    %d \r\n", message_len);
+}
+
+
+int main(){
+
+  uint8_t display_packet[] = {0x1, 0x34, 0x05, 0x0, 0x48, 0x65, 0x6c, 0x6c, 0x6f};
+  uint8_t motor_packet[] = {0x2, 0x80, 0x08, 0x0, 0x0, 0x0, 0x80, 0x3f, 0x0, 0x0, 0x0, 0xbf};
+
+  dispatcher(display_packet);
+
+  printf("Done dispatcher \r\n");
 
   return 0;
 }
