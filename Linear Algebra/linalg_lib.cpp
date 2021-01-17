d328fbdb13e77476a013059f7a7aa8c4e6732129
Sheil Sarda <sheilsarda@yahoo.com>
2021-01-17 00:40:34 -0500

    handles rect matrices

--- Linear Algebra/linalg_lib.cpp
+++ Linear Algebra/linalg_lib.cpp
@@ -38,9 +38,15 @@ class Matrix {
 class MatrixOps {
   public:
   
-    Matrix transpose(Matrix m){
+    Matrix transpose(Matrix a){
       
-        return m;
+      Matrix res = Matrix(a.n_cols, a.n_rows);
+
+      for(int i = 0; i < a.n_rows; ++i)
+	for(int j = 0; j < a.n_cols; ++j)
+	  res.m[j][i] = a.m[i][j];
+
+      return res;
       
     }
 
@@ -53,15 +59,61 @@ class MatrixOps {
 int main() {
  
   MatrixOps ops;
+
+  /**
+   * Test 1: Symmetric Matrix:
+   * transpose should be the same as original
+   */
+
   Matrix m1(2, 2);
   
   m1.m[0][0] = 1;
+  m1.m[0][1] = 2;
+  m1.m[1][0] = 2;
   m1.m[1][1] = 1;
-  
+
   Matrix m3 = ops.transpose(m1);
+
+  cout << "Test 1: Symmetric Matrix" << endl;
+  m1.print();
   m3.print();
 
-  Matrix m2(2, 2);
+
+  /**
+   * Test 2: Asymmetric Matrix:
+   * transpose should not be the same as original
+   */
+
+  m1.m[0][0] = 1;
+  m1.m[0][1] = 2;
+  m1.m[1][0] = 3;
+  m1.m[1][1] = 4;
+
+  m3 = ops.transpose(m1);
+  
+  cout << "Test 2: Asymmetric Matrix" << endl;
+  m1.print();
+  m3.print();
+  
+  /**
+   * Test 3: Rectangular Matrix:
+   * transpose should not be the same as original
+   */
+
+  Matrix m2(3, 5);
+
+  int val = 1;
+  for(int i = 0; i < m2.n_rows; ++i)
+    for(int j = 0; j < m2.n_cols; ++j)
+      m2.m[i][j] = val++;
+
+  m3 = ops.transpose(m2);
+  
+  cout << "Test 3: Rectangular Matrix" << endl;
+  m2.print();
+  m3.print();
+
+
 
   return 0;
 }
