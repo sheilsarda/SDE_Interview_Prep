fbd2fa9a12a3a7bd305060e22d3c5d9c43b04e7e
Sheil Sarda <sheilsarda@yahoo.com>
2021-01-16 23:51:57 -0500

    segfault when accessing array

--- Linear Algebra/linalg_lib.cpp
+++ Linear Algebra/linalg_lib.cpp
@@ -2,6 +2,7 @@
 
 #include<vector>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -11,8 +12,7 @@ class Matrix {
     int n_cols;
     int **m;
     
-    Matrix(int r, int c) :
-      n_rows(r), n_cols(c) {
+    Matrix(int r, int c) : n_rows(r), n_cols(c) {
    
       m = (int **) malloc(n_rows * n_cols * sizeof(int));
 
@@ -20,6 +20,16 @@ class Matrix {
         for(int j = 0; j < n_cols; ++j)
 	  m[i][j] = 0;
 
+    }
+
+    void print() {
+      cout << "M: " << endl;
+      for(int i = 0; i < n_rows; ++i){
+        for(int j = 0; j < n_cols; ++j)
+	  cout << setw(3) << m[i][j] << " ";
+	cout << endl;
+      }
+      
     }
 };
 
@@ -39,7 +49,19 @@ class MatrixOps {
 };
 
 int main() {
-  
+ 
+  MatrixOps ops;
+  Matrix m1(2, 2);
+  m1.m[0][0] = 1;
+  m1.m[1][1] = 1;
+/*
+
+  Matrix m3 = ops.transpose(m1);
+  m3.print();
+
+  Matrix m2(2, 2);
+
+*/
   return 0;
 }
 
