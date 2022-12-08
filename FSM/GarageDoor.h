2e993952b38b6abe8036311c1f1dd8d0c5d8fb75
sheilsarda <sheilsarda@yahoo.com>
2022-12-08 04:43:23 -0500

    added timer functionality

--- FSM/GarageDoor.h
+++ FSM/GarageDoor.h
@@ -1,21 +1,27 @@
 #ifndef GarageDoorClass
 #define GarageDoorClass
 
+#define doorCloseTime   15000; // milliseconds to close door
+#define doorOpenTime    15000; // milliseconds to open door
+
 class GarageDoor {
     enum DoorState{
         Closed=0, 
         Open=1, 
         Start_Opening=2, 
-        Start_Closing=3
+        Start_Closing=3,
+        Freeze=-1
     };
 
     private:
         DoorState currentState, prevState;
+        uint16_t actionCounter, currentTime;
 
     public:
         GarageDoor();
         DoorState doorTriggered();
         DoorState safetyTrigger();
+        void timerCompare();
 
 };
 
--- FSM/main.cpp
+++ FSM/main.cpp
@@ -1,18 +1,50 @@
 #include "GarageDoor.h"
 #include <iostream>
 
-GarageDoor::GarageDoor():currentState(Closed), prevState(Closed) {
+GarageDoor::GarageDoor():currentState(Closed), prevState(Closed), actionCounter(0) {
 }
 
 GarageDoor::DoorState GarageDoor::doorTriggered(){
+    switch(GarageDoor::currentState){
+        case Closed: 
+            GarageDoor::prevState       = Closed;
+            GarageDoor::currentState    = Start_Opening;
+            GarageDoor::actionCounter   = GarageDoor::currentTime;
+            break;
+        case Open:
+            GarageDoor::prevState       = Open;
+            GarageDoor::currentState    = Start_Closing;
+            GarageDoor::actionCounter   = GarageDoor::currentTime;
+            break;            
+        case Start_Opening:
+            GarageDoor::prevState       = Start_Opening;
+            GarageDoor::currentState    = Freeze;   
+            GarageDoor::actionCounter   = 0; 
+            break;            
+        case Start_Closing:
+            GarageDoor::prevState       = Start_Closing;
+            GarageDoor::currentState    = Freeze;  
+            GarageDoor::actionCounter   = 0; 
+            break;            
+        case Freeze:
+            if(GarageDoor::prevState == Start_Opening) GarageDoor::currentState = Start_Closing;
+            else GarageDoor::currentState = Start_Opening;
+            GarageDoor::actionCounter   = GarageDoor::currentTime;
+            GarageDoor::prevState       = Freeze;
+    }
     return GarageDoor::currentState;
 }
 GarageDoor::DoorState GarageDoor::safetyTrigger(){
     return GarageDoor::currentState;
 }
 
+void GarageDoor::timerCompare(){
+}
+
+
 int main(){
     std::cout << "Hello World\r\n";
     GarageDoor door;
+    
     std::cout << door.doorTriggered() << "\r\n";
 }
