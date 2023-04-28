4561107fbbbc413cfb9decaafd16f1be3cce4436
sheilsarda <sheilsarda@yahoo.com>
2023-04-28 09:18:48 -0400

    finished making changes to main

--- Navigation/main.py
+++ Navigation/main.py
@@ -2,7 +2,7 @@ import numpy as np
 from dynamic_prog_sol import DPGridTraversal
 from brute_force_sol import BruteForceGridTraversal
 
-tests_to_run = [1]
+tests_to_run = [1, 2, 3, 4, 5] # idxs of tests to run (located in tests subdirectory)
 
 input_file_str = "tests/test_input_"
 output_file_str = "tests/test_output_"
@@ -15,18 +15,23 @@ def main():
         input_filename = input_file_str + str(test_idx) + ".txt"
         output_filename = output_file_str + str(test_idx) + ".txt"
 
+        print("Starting test " + str(test_idx))
+
         if approach == "dynamic_prog":
             # Dynamic Programming Soln
             dp_soln = DPGridTraversal(input_filename, output_filename)
             path_len, path = dp_soln.determine_best_path()
 
             print("Path length: ", path_len)
-            print(np.matrix([dp_soln.__avocado_positions__[i-1] for i in path]).transpose())
+            print(np.fliplr(np.matrix([dp_soln.__avocado_positions__[i-1] for i in path]).transpose()))
 
         else:
             # Brute Force Soln
             bf_soln = BruteForceGridTraversal(input_filename, output_filename)
             bf_soln.determine_best_path()
+
+        print("Finished Running test " + str(test_idx))
+        print("--------------------------------------")
 
 if __name__ == "__main__":
     main()
